d5d06944312db63e5720087fa0713a207adba24f
ievonzas <ievastanelyte@yahoo.com>
2025-03-25 16:49:26 +0200

    war_cardgame.py

--- war_cardgame.py
+++ war_cardgame.py
@@ -1,2 +1,19 @@
-#ale kas vyksta :)
-#ale programinam
+import random
+
+class Suits:
+
+    SYMBOLS = {"clubs": "♣", "diamonds":"♦", "hearts" : "♥", "spades" : "♠"}
+    def __init__(self, description):
+            self._description = description
+            self._symbol = Suits.SYMBOLS[description.lower()]
+    
+    # getter for description
+    @property
+    def description (self):
+          return self._description
+    
+    # getter for symbol
+
+    @property
+    def symbol(self):
+          return self._symbol
